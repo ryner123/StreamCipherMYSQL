6e96aace8d66925796579f158acc8a8f994e3696
Ryner <33190126+ryner123@users.noreply.github.com>
2017-10-29 12:16:54 +0800

    ACTUAL FILES

---
+++ cipher/CipherKeysShit.php
@@ -0,0 +1,177 @@
+<?php 
+
+//KEYS
+
+$stringHereCaps = 'ASSWHOLEPIECEOFSHIT';
+$stringHereSmall = 'abcdefghijklmnopqrstuvwxyz';
+//a    1     26
+if($shityWorld[$ctr]=='a'){$crazyPass[$ctr] = $stringLEngth.'z'.'L?';}
+//b    2     25
+elseif($shityWorld[$ctr]=='b'){$crazyPass[$ctr] = $stringLEngth.'y'.'L?';}
+//c    3     24
+elseif($shityWorld[$ctr]=='c'){$crazyPass[$ctr] = $stringLEngth.'x'.'L?';}
+//d    4     23
+elseif($shityWorld[$ctr]=='d'){$crazyPass[$ctr] = $stringLEngth.'w'.'L?';}
+//e    5     22
+elseif($shityWorld[$ctr]=='e'){$crazyPass[$ctr] = $stringLEngth.'v'.'L?';}
+//f    6     21
+elseif($shityWorld[$ctr]=='f'){$crazyPass[$ctr] = $stringLEngth.'u'.'L?';}
+//g    7     20
+elseif($shityWorld[$ctr]=='g'){$crazyPass[$ctr] = $stringLEngth.'t'.'L?';}
+//h    8     19
+elseif($shityWorld[$ctr]=='h'){$crazyPass[$ctr] = $stringLEngth.'s'.'L?';}
+//i    9     18
+elseif($shityWorld[$ctr]=='i'){$crazyPass[$ctr] = $stringLEngth.'r'.'L?';}
+//j    10    17
+elseif($shityWorld[$ctr]=='j'){$crazyPass[$ctr] = $stringLEngth.'q'.'L?';}
+//k    11    16
+elseif($shityWorld[$ctr]=='k'){$crazyPass[$ctr] = $stringLEngth.'p'.'L?';}
+//l    12    15
+elseif($shityWorld[$ctr]=='l'){$crazyPass[$ctr] = $stringLEngth.'o'.'L?';}
+//m    13    14
+elseif($shityWorld[$ctr]=='m'){$crazyPass[$ctr] = $stringLEngth.'n'.'L?';}
+//n    14    13
+elseif($shityWorld[$ctr]=='n'){$crazyPass[$ctr] = $stringLEngth.'m'.'L?';}
+//o    15    12
+elseif($shityWorld[$ctr]=='o'){$crazyPass[$ctr] = $stringLEngth.'l'.'L?';}
+//p    16    11
+elseif($shityWorld[$ctr]=='p'){$crazyPass[$ctr] = $stringLEngth.'k'.'L?';}
+//q    17    10
+elseif($shityWorld[$ctr]=='q'){$crazyPass[$ctr] = $stringLEngth.'j'.'L?';}
+//r    18    9
+elseif($shityWorld[$ctr]=='r'){$crazyPass[$ctr] = $stringLEngth.'i'.'L?';}
+//s    19    8
+elseif($shityWorld[$ctr]=='s'){$crazyPass[$ctr] = $stringLEngth.'h'.'L?';}
+//t    20    7
+elseif($shityWorld[$ctr]=='t'){$crazyPass[$ctr] = $stringLEngth.'g'.'L?';}
+//u    21    6
+elseif($shityWorld[$ctr]=='u'){$crazyPass[$ctr] = $stringLEngth.'f'.'L?';}
+//v    22    5
+elseif($shityWorld[$ctr]=='v'){$crazyPass[$ctr] = $stringLEngth.'e'.'L?';}
+//w    23    4
+elseif($shityWorld[$ctr]=='w'){$crazyPass[$ctr] = $stringLEngth.'d'.'L?';}
+//x    24    3
+elseif($shityWorld[$ctr]=='x'){$crazyPass[$ctr] = $stringLEngth.'c'.'L?';}
+//y    25    2
+elseif($shityWorld[$ctr]=='y'){$crazyPass[$ctr] = $stringLEngth.'b'.'L?';}
+//z    26    1
+elseif($shityWorld[$ctr]=='z'){$crazyPass[$ctr] = $stringLEngth.'a'.'L?';}
+
+//A    1     26
+elseif($shityWorld[$ctr]=='A'){$crazyPass[$ctr] = $stringLEngth.'Z'.'s?';}
+//B    2     25
+elseif($shityWorld[$ctr]=='B'){$crazyPass[$ctr] = $stringLEngth.'Y'.'s?';}
+//C    3     24
+elseif($shityWorld[$ctr]=='C'){$crazyPass[$ctr] = $stringLEngth.'X'.'s?';}
+//D    4     23
+elseif($shityWorld[$ctr]=='D'){$crazyPass[$ctr] = $stringLEngth.'W'.'s?';}
+//E    5     22
+elseif($shityWorld[$ctr]=='E'){$crazyPass[$ctr] = $stringLEngth.'V'.'s?';}
+//F    6     21
+elseif($shityWorld[$ctr]=='F'){$crazyPass[$ctr] = $stringLEngth.'U'.'s?';}
+//G    7     20
+elseif($shityWorld[$ctr]=='G'){$crazyPass[$ctr] = $stringLEngth.'T'.'s?';}
+//H    8     19
+elseif($shityWorld[$ctr]=='H'){$crazyPass[$ctr] = $stringLEngth.'S'.'s?';}
+//I    9     18
+elseif($shityWorld[$ctr]=='I'){$crazyPass[$ctr] = $stringLEngth.'R'.'s?';}
+//J    10    17
+elseif($shityWorld[$ctr]=='J'){$crazyPass[$ctr] = $stringLEngth.'Q'.'s?';}
+//K    11    16
+elseif($shityWorld[$ctr]=='K'){$crazyPass[$ctr] = $stringLEngth.'P'.'s?';}
+//L    12    15
+elseif($shityWorld[$ctr]=='L'){$crazyPass[$ctr] = $stringLEngth.'O'.'s?';}
+//M    13    14
+elseif($shityWorld[$ctr]=='M'){$crazyPass[$ctr] = $stringLEngth.'N'.'s?';}
+//N    14    13
+elseif($shityWorld[$ctr]=='N'){$crazyPass[$ctr] = $stringLEngth.'M'.'s?';}
+//O    15    12
+elseif($shityWorld[$ctr]=='O'){$crazyPass[$ctr] = $stringLEngth.'L'.'s?';}
+//P    16    11
+elseif($shityWorld[$ctr]=='P'){$crazyPass[$ctr] = $stringLEngth.'K'.'s?';}
+//Q    17    10
+elseif($shityWorld[$ctr]=='Q'){$crazyPass[$ctr] = $stringLEngth.'J'.'s?';}
+//R    18    9
+elseif($shityWorld[$ctr]=='R'){$crazyPass[$ctr] = $stringLEngth.'I'.'s?';}
+//S    19    8
+elseif($shityWorld[$ctr]=='S'){$crazyPass[$ctr] = $stringLEngth.'H'.'s?';}
+//T    20    7
+elseif($shityWorld[$ctr]=='T'){$crazyPass[$ctr] = $stringLEngth.'G'.'s?';}
+//U    21    6
+elseif($shityWorld[$ctr]=='U'){$crazyPass[$ctr] = $stringLEngth.'F'.'s?';}
+//V    22    5
+elseif($shityWorld[$ctr]=='V'){$crazyPass[$ctr] = $stringLEngth.'E'.'s?';}
+//W    23    4
+elseif($shityWorld[$ctr]=='W'){$crazyPass[$ctr] = $stringLEngth.'D'.'s?';}
+//X    24    3
+elseif($shityWorld[$ctr]=='X'){$crazyPass[$ctr] = $stringLEngth.'C'.'s?';}
+//Y    25    2
+elseif($shityWorld[$ctr]=='Y'){$crazyPass[$ctr] = $stringLEngth.'B'.'s?';}
+//Z    26    1
+elseif($shityWorld[$ctr]=='Z'){$crazyPass[$ctr] = $stringLEngth.'A'.'s?';}
+
+
+//1
+elseif($shityWorld[$ctr]=='1'){$crazyPass[$ctr] = 1*$stringLEngth.'?';}
+//2
+elseif($shityWorld[$ctr]=='2'){$crazyPass[$ctr] = 2*$stringLEngth.'?';}
+//3
+elseif($shityWorld[$ctr]=='3'){$crazyPass[$ctr] = 3*$stringLEngth.'?';}
+//4
+elseif($shityWorld[$ctr]=='4'){$crazyPass[$ctr] = 4*$stringLEngth.'?';}
+//5
+elseif($shityWorld[$ctr]=='5'){$crazyPass[$ctr] = 5*$stringLEngth.'?';}
+//6
+elseif($shityWorld[$ctr]=='6'){$crazyPass[$ctr] = 6*$stringLEngth.'?';}
+//7
+elseif($shityWorld[$ctr]=='7'){$crazyPass[$ctr] = 7*$stringLEngth.'?';}
+//8
+elseif($shityWorld[$ctr]=='8'){$crazyPass[$ctr] = 8*$stringLEngth.'?';}
+//9
+elseif($shityWorld[$ctr]=='9'){$crazyPass[$ctr] = 9*$stringLEngth.'?';}
+
+//@
+elseif($shityWorld[$ctr]=='@'){$crazyPass[$ctr] = '@?';}
+//#
+elseif($shityWorld[$ctr]=='#'){$crazyPass[$ctr] = '#?';}
+//$
+elseif($shityWorld[$ctr]=='$'){$crazyPass[$ctr] = '$?';}
+//%
+elseif($shityWorld[$ctr]=='%'){$crazyPass[$ctr] = '%?';}
+//^
+elseif($shityWorld[$ctr]=='^'){$crazyPass[$ctr] = '^?';}
+//&
+elseif($shityWorld[$ctr]=='&'){$crazyPass[$ctr] = '&?';}
+//*
+elseif($shityWorld[$ctr]=='*'){$crazyPass[$ctr] = '*?';}
+//(
+elseif($shityWorld[$ctr]=='('){$crazyPass[$ctr] = '(?';}
+//)
+elseif($shityWorld[$ctr]==')'){$crazyPass[$ctr] = ')?';}
+//-
+elseif($shityWorld[$ctr]=='-'){$crazyPass[$ctr] = '-?';}
+//_
+elseif($shityWorld[$ctr]=='_'){$crazyPass[$ctr] = '_?';}
+//=
+elseif($shityWorld[$ctr]=='='){$crazyPass[$ctr] = '=?';}
+//+
+elseif($shityWorld[$ctr]=='+'){$crazyPass[$ctr] = '+?';}
+//>
+elseif($shityWorld[$ctr]=='>'){$crazyPass[$ctr] = '>?';}
+//<
+elseif($shityWorld[$ctr]=='<'){$crazyPass[$ctr] = '<?';}
+//space
+elseif($shityWorld[$ctr]==' '){$crazyPass[$ctr] = 'SP?';}
+//others
+else{
+		
+		$errorHere = "SOMETHINGWENTWRONG";
+		header("location:/StreamCipher/babe/cipher/NotepadReg.php");
+	
+}
+
+
+
+
+
+?>
